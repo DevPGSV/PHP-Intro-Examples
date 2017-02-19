94b8b4d068fe70cf01d965a330e89aef35751e97
DevPGSV <DevPGSV@gmail.com>
2017-02-19 20:21:19 +0100

    Modified 4.php: added associative arrays

--- 4.php
+++ 4.php
@@ -10,4 +10,22 @@ $names = [
 echo count($names)."\n\n";
 echo $names[2]."\n\n";
 print_r($names);
+echo "\n\n\n";
+
+
+$assoc = [
+    "data" => "value",
+    "moredata" => 1,
+    "somethingelse" => [
+        1, 2, 3
+    ],
+    "lastthing" => [
+        "elem1" => 1,
+        "elem2" => 2,
+        "elem3" => 3
+    ],
+];
+
+echo $assoc['lastthing']['elem2'] . "\n\n";
+print_r($assoc);
 
